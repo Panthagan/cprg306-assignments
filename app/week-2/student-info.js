ef8665d12bdb3440665eed0cb5f8dfc6200d4056
Apoorva <131406245+Panthagan@users.noreply.github.com>
2024-01-30 22:21:14 -0700

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+ 
+const StudentInfo = () => {
+  return (
+    <div>
+        <a href='https://github.com/Panthagan'> my github </a>
+      <Link href="https://github.com">
+      </Link>
+    </div>
+  );
+};
+ 
+export default StudentInfo;
