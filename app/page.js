f52c384f66a0374594702b2b7beffddc20800244
Apoorva <131406245+Panthagan@users.noreply.github.com>
2024-03-11 13:00:02 -0600

    week-7

--- app/page.js
+++ app/page.js
@@ -11,7 +11,8 @@ export default function Page() {
         <li><Link href="/week-4">Week 4 </Link></li> 
         <li><Link href="/week-5">Week 5 </Link></li> 
         <li><Link href="/week-6">Week 6 </Link></li> 
-
+        <li><Link href="/week-7">Week 7 </Link></li> 
+        
       </ul>
     </div>
     </main>
